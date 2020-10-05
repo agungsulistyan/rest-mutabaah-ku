4bb5cfe905228c5f3940402857f2ffe70a5f28c7
Agung Sulistyanto <agung.sulistyan@gmail.com>
2020-10-05 08:32:21 +0700

    C:/Program Files/Git/login x-www-form-urlencoded

--- src/main/java/com/sulistyan/mutabaahku/controller/LoginController.java
+++ src/main/java/com/sulistyan/mutabaahku/controller/LoginController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpMessage;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
@@ -19,7 +21,25 @@ public class LoginController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/login")
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<?> login(@RequestParam Map<String, String> body) {
+
+        User user = userRepository.findUserByUsernamePassword(body.get("username"), body.get("password"));
+
+        if(user == null){
+            throw new ResourceNotFoundException("Gagal Login :: " + body.get("username"));
+        }
+
+        HashMap<String, Object> hmap = new HashMap<String, Object>();
+        hmap.put("status", HttpStatus.OK.value());
+        hmap.put("message", "success");
+        hmap.put("user", user);
+
+        return new ResponseEntity<HashMap<String, Object>>(hmap, HttpStatus.OK);
+    }
+
+    /*
+    @PostMapping(value = "/login")
     public ResponseEntity<?> login(@Valid @RequestBody User userParam) {
 
         User user = userRepository.findUserByUsernamePassword(userParam.getUsername(), userParam.getPassword());
@@ -35,4 +55,5 @@ public class LoginController {
 
         return new ResponseEntity<HashMap<String, Object>>(hmap, HttpStatus.OK);
     }
+    * */
 }
